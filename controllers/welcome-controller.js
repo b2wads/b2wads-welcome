6bca9bfeb049ec6f0687cb4c194f2fcbcd9273d9
Hugo Nogueira <nogueiradeoliveira@gmail.com>
2020-10-21 17:13:24 -0300

    implements welcome controller

--- controllers/welcome-controller.js
+++ controllers/welcome-controller.js
@@ -1,12 +1,9 @@
+const WelcomeService = require("../services/welcome-service")
+
 const WelcomeController = {
   async welcome(req, res) {
-    
-    /**
-     * Implemente um retorno JSON asíncrono
-     * com código 200, que chama o método welcome()
-     * de services/welcome-service.js
-     */
-    
+    const welcome = await WelcomeService.welcome()
+    await res.status(200).json(welcome)
   }
 }
 
