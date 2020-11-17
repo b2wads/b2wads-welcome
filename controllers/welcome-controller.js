5951f2aca199ec2b5e9e5d4240a2c1b33f09c2da
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2020-11-17 17:24:52 -0300

    implemented return JSON async in call welcome() method

--- controllers/welcome-controller.js
+++ controllers/welcome-controller.js
@@ -1,3 +1,5 @@
+const WelcomeService = require("../services/welcome-service")
+
 const WelcomeController = {
   async welcome(req, res) {
     
@@ -6,6 +8,10 @@ const WelcomeController = {
      * com código 200, que chama o método welcome()
      * de services/welcome-service.js
      */
+
+     const welcomeData = await WelcomeService.welcome();
+
+     return res.status(200).json(welcomeData);
     
   }
 }
